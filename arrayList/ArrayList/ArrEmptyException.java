ef48f0da23850e45f75e493a971f2265f07192b1
qyx101 <xmqyx1129@gmail.com>
2023-12-03 14:05:34 +0800

    顺序表的实现

---
+++ arrayList/ArrayList/ArrEmptyException.java
@@ -0,0 +1,10 @@
+package ArrayList;
+
+public class ArrEmptyException extends RuntimeException{
+    public ArrEmptyException() {
+    }
+
+    public ArrEmptyException(String message) {
+        super(message);
+    }
+}
