468f2558819791abfad091fae04da370f7e084e2
qyx101 <xmqyx1129@gmail.com>
2023-12-04 10:08:23 +0800

    未完顺序表-12-3

--- 12-3/MySingleList/MySingleList.java
+++ 12-3/MySingleList/MySingleList.java
@@ -83,100 +83,37 @@ public class MySingleList implements IList {
         ListNode node = new ListNode(data);
         ListNode cur = this.head;
 
-        // 1. 判断index位置是否合法
-        if (index < 0 || index > this.size()) {
-            throw new IndexException(index +" 是非法的indexs位置 重新输入");
-        }
-        // 2. 处理特殊情况1: 链表没有节点
-        if (this.head == null) {
-            this.head = node;
-            return;
-        }
-        // 3. 处理特殊情况2: 头插
-        if (0 == index) {
-            node.next = this.head;
-            this.head = node;
-            return;
-        }
-
-        // 正常情况: 尾/中间插入
         while (index-1 > 0) {
             cur = cur.next;
             index--;
         }
+
         node.next = cur.next;
         cur.next = node;
+
     }
 
 
-    // 删除第一次出现数据为key的节点
+    // 删除第一次出现关键字为key的节点
     @Override
     public void remove(int key) {
 
-        // 处理特殊情况1: 链表没有节点
-        if (this.size() == 0) {
-            System.out.println("链表为空 不能进行删除操作");
-            return;
-        }
-        // 处理特殊情况2: 头删
-        if (this.head.value == key) {
-            this.head = this.head.next;
-            return;
-        }
-
-        ListNode cur = this.head;
-        ListNode prev = findPrevKey(cur,key);
-        if (prev != null) {
-            prev.next = prev.next.next;
-        }else {
-            System.out.println("没有你要删除的数据: "+key);
-            System.out.println("删除失败");
-        }
-    }
-    private ListNode findPrevKey(ListNode cur,int key) {
-        while (cur.next != null) {
-            if (cur.next.value == key) {
-                return cur;
-            }else {
-                cur = cur.next;
-            }
-        }
-        return null;
     }
 
     // 删除所有值为key的节点
     @Override
     public void removeAllKey(int key) {
-        // 处理特殊情况1: 链表没有节点
-        if (this.head == null) {
-            System.out.println("链表为空 不能进行删除操作");
-            return;
-        }
 
-        // 删除 除了第一个节点
-        ListNode prev = this.head;
-        ListNode cur = prev.next;
-
-        while (cur != null) {
-            if (cur.value == key) {
-                prev.next = cur.next;
-                cur = cur.next;
-            }else {
-                prev = cur;
-                cur = cur.next;
-            }
-        }
-
-        // 处理特殊情况2: 判断第一个节点
-        if (this.head.value == key) {
-            this.head = this.head.next;
-        }
     }
 
 
     @Override
     public void clear() {
-        this.head = null;
+
     }
 
+
+    /* public ListNode newListNode(int val) {
+
+    }*/
 }
--- MySingleList/Test.java
+++ MySingleList/Test.java
@@ -9,7 +9,7 @@ public class Test {
         mySingleList.addLast(2);
         mySingleList.addLast(3);
         mySingleList.addLast(4);*/
-        mySingleList.display();
+        //mySingleList.display();
 
         // 测试头插
         /*mySingleList.addFirst(1);
@@ -20,10 +20,35 @@ public class Test {
         mySingleList.addFirst(3);
         mySingleList.display();*/
 
-        mySingleList.addLast(1);
+        /*mySingleList.addLast(1);
         mySingleList.addLast(2);
         mySingleList.addLast(3);
-        mySingleList.addIndex(1,4);
+        mySingleList.addIndex(1,4);*/
+
+        // 测试  // index任意位置插入
+       /* mySingleList.addLast(1);
+        mySingleList.addLast(2);
+        mySingleList.addLast(3);
+        // 测试头插
+        mySingleList.addIndex(0,4);
+        mySingleList.display();
+        // 测试中间插入
+        mySingleList.addIndex(2,5);
+        mySingleList.display();
+        // 测试尾插
+        mySingleList.addIndex(5,6);
+        mySingleList.display();*/
+
+        // 测试删除
+        mySingleList.addLast(2);
+        mySingleList.addLast(2);
+        mySingleList.addLast(2);
+        mySingleList.addLast(2);
+        mySingleList.addLast(2);
+        //mySingleList.remove(1);
+        mySingleList.display();
+        mySingleList.removeAllKey(2);
+        mySingleList.removeAllKey(1);
         mySingleList.display();
 
 
