227ac105d27ac1d722a2d696a8d25de6ee8fc613
qyx101 <xmqyx1129@gmail.com>
2023-12-08 06:33:08 +0800

    队列面试题 12-8

--- QueueOJ/MyQueue.java
+++ QueueOJ/MyQueue.java
@@ -2,6 +2,49 @@ package QueueOJ;
 
 // 两个栈实现队列
 
-public class MyQueue {
+import java.util.Stack;
 
+class MyQueue {
+    public Stack<Integer> pushStack;
+    public Stack<Integer> popStack;
+
+    public MyQueue() {
+        this.pushStack = new Stack<>();
+        this.popStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        this.pushStack.push(x);
+    }
+
+    public int pop() {
+        if (this.empty())
+            return -1;
+
+        if (this.popStack.isEmpty()) {
+            while (!this.pushStack.isEmpty()) {
+                this.popStack.push(pushStack.pop());
+            }
+        }
+        return this.popStack.pop();
+    }
+
+    public int peek() {
+        if (this.empty())
+            return -1;
+
+        if (this.popStack.isEmpty()) {
+            while (!this.pushStack.isEmpty()) {
+                this.popStack.push(pushStack.pop());
+            }
+        }
+        return this.popStack.peek();
+    }
+
+    public boolean empty() {
+        return this.pushStack.isEmpty() && this.popStack.isEmpty();
+    }
 }
+
+
+
--- QueueOJ/Test.java
+++ QueueOJ/Test.java
@@ -3,9 +3,12 @@ package QueueOJ;
 public class Test {
     public static void main(String[] args) {
         //MyCircularQueue myCircularQueue = new MyCircularQueue(3);
-        MyStack myStack = new MyStack();
+       /* MyStack myStack = new MyStack();
         myStack.push(1);
         myStack.pop();
-        System.out.println(myStack.empty());
+        System.out.println(myStack.empty());*/
+
+        MyQueue myQueue = new MyQueue();
+
     }
 }
