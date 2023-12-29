e02efa5b1fc8033e4ca6ee7621c61b9b636fbf9a
qyx101 <xmqyx1129@gmail.com>
2023-12-29 20:49:03 +0800

    12-29 -二叉树面试题

--- 12-29/Test.java
+++ 12-29/Test.java
@@ -45,3 +45,49 @@ class Solution {
         return -1;
     }
 }
+
+2. 从中序与后序遍历序列构造二叉树
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+
+    public int postIndex;
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        this.postIndex = postorder.length-1;
+        return buildTreeChild(inorder,postorder,0,inorder.length-1);
+    }
+
+    public TreeNode buildTreeChild(int[] inorder,int[] postorder,int inBegin,int inEnd) {
+        if (inBegin > inEnd) {
+            return null;
+        }
+        TreeNode root = new TreeNode(postorder[postIndex]);
+        int rootIndex = findIndexRoot(inorder,0,inorder.length,root.val);
+        postIndex--;
+        // 必须先创建右树 因为后序遍历是左右根, 下一个根是右子树的根
+        root.right = buildTreeChild(inorder,postorder,rootIndex+1,inEnd);
+        root.left = buildTreeChild(inorder,postorder,inBegin,rootIndex-1);
+        return root;
+    }
+    public int findIndexRoot(int[] inorder,int inBegin,int inEnd,int key) {
+        for (int i = inBegin;i < inEnd;i++) {
+            if (inorder[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
